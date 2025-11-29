350d25ea97d9e56fbded2248b4ce5f5c3181ca67
Rishabh Tewari <rishabhtewari1@gmail.com>
2025-11-29 12:24:38 -0800

    Update training parameters in clip.py: reduce num_train_epochs to 1, increase per_device_train_batch_size to 256, and set num_workers to 16 for optimized performance.

--- homework/clip.py
+++ homework/clip.py
@@ -258,12 +258,12 @@ def get_target_modules_for_lora(model: nn.Module) -> list[str]:
 def train(
     data_dir: Path | None = None,
     output_dir: str = "clip",
-    num_train_epochs: float = 5,  # 0.05 for debugging purpose, increase this once the dry run works
+    num_train_epochs: float = 1,  # 0.05 for debugging purpose, increase this once the dry run works
     # per_device_train_batch_size: int = 1024,
-    per_device_train_batch_size: int = 128,
+    per_device_train_batch_size: int = 256,
     gradient_accumulation_steps: int = 1,
     learning_rate: float = 5e-4,
-    num_workers: int = 2,
+    num_workers: int = 16,
 ):
     vlm = BaseVLM()
 
