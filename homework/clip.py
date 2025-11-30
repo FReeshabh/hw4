ff9c571432bf3434bbd65ef5a427589f57463ada
Rishabh Tewari <rishabhtewari1@gmail.com>
2025-11-29 20:52:47 -0800

    2 epochs

--- homework/clip.py
+++ homework/clip.py
@@ -258,7 +258,7 @@ def get_target_modules_for_lora(model: nn.Module) -> list[str]:
 def train(
     data_dir: Path | None = None,
     output_dir: str = "clip",
-    num_train_epochs: float = 1.7,  # 0.05 for debugging purpose, increase this once the dry run works
+    num_train_epochs: float = 2.0,  # 0.05 for debugging purpose, increase this once the dry run works
     per_device_train_batch_size: int = 512,
     gradient_accumulation_steps: int = 1,
     learning_rate: float = 5e-4,
